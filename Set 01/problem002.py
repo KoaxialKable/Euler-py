a583aab72f2fc28b3305e901744f7a6015d7577a
Brian <A01879470@b118comp32.aggies.usu.edu>
2014-04-23 14:57:49 -0600

    Updated comment headers

--- Set 01/problem002.py
+++ Set 01/problem002.py
@@ -10,6 +10,7 @@
 #	By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
 #		find the sum of the even-valued terms.
 #
+#	Answer: 4613732
 ############################################
 sum,a,b = 0,1,2
 while (a <= 4000000):
--- Set 01/problem004.py
+++ Set 01/problem004.py
@@ -7,16 +7,17 @@
 #	The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #	Find the largest palindrome made from the product of two 3-digit numbers.
 #
+#	Answer: 906609  913 * 993
 ############################################
 pals = []
 def isPalindrome(n):
 	if (str(n) == str(n)[::-1]):
 		return True
 	return False
-	
+
 for i in range(999,99,-1):
 	for j in range(999,99,-1):
 			if(isPalindrome(i*j)):
 				pals.append(i*j)
-				
+
 print(sorted(pals)[-1])
