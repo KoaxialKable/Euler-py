d36c95791c92cb3e9127d7dbb8782da0867bf663
Koaxialkable <A01879470@b118comp01.aggies.usu.edu>
2014-04-24 16:53:12 -0600

    p13 header only

---
+++ Set 01/problem013.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+#######################################
+#	Problem 13
+#	Large sum
+#
+#	Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+#
+#	Answer: 
+#######################################
