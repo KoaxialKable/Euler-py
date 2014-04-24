934ea20436c31d0693cd7aa7d9dc64057bc6738b
unknown <A01879470@b107comp06.aggies.usu.edu>
2014-04-24 08:54:24 -0600

    notes on 9

---
+++ Set 01/problem009.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+############################################
+#	Problem 6
+#	Sum square difference
+#
+#	A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#				a**2 + b**2 = c**2
+#
+#	For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2.
+#
+#	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+#	Find the product abc.
+#
+#	Answer: 
+############################################
+a,b,c = 1,2,3
+
+def play():
+	for a in range(1,333):
+
+# a can be anywhere from 1 - 333
+# b can be anywhere from 2 - 499
+# c can be ahywhere from 3 - 500
