6e7b0cd7de7461c012435791bfde6903ca4c9e58
Koaxialkable <koaxialkable@gmail.com>
2015-02-25 11:48:26 -0700

    Added commend-header

--- Set 04/problem456.py
+++ Set 04/problem456.py
@@ -1,4 +1,27 @@
 #! /usr/bin/env python
+############################################
+#	Problem 456
+#	Triangles containing the origin II
+#
+#	Define:
+#	x(n) = (1248^n mod 32323) - 16161
+#	y(n) = (8421^n mod 30103) - 15051
+#	P(n) = {(x1, y1), (x2, y2), ..., (xn, yn)}
+#
+#	For example, P(8) = {(-14913, -6630), (-10161, 5625), (5226, 11896), (8340, -10778), 
+#					  (15852, -5203), (-15165, 11295), (-1427, -14495), (12407, 1060)}.
+#
+#	Let C(n) be the number of triangles whose vertices are in P(n) which contain the origin in the interior.
+#
+#	Examples:
+#	C(8) = 20
+#	C(600) = 8950634
+#	C(40 000) = 2666610948988
+#
+#	Find C(2 000 000)
+#
+#	Answer: 
+############################################
 points_list = []
 
 def getPoints(count):
