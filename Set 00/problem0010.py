02763b71c26ef3375e9bc173dee1b73589970780
Koaxialkable <koaxialkable@gmail.com>
2015-03-10 11:57:49 -0600

    Re-wrote solution, increased performance

--- Set 00/problem0010.py
+++ Set 00/problem0010.py
@@ -9,7 +9,30 @@
 #
 #	Answer: 142913828922
 ############################################
-sum = 2
-for i in range(3,2000000,2):
-	sum += {True: 0, False: i}[any(i % j == 0 for j in range(3, int(i**0.5) + 1, 2))]
-print(sum)
+#	Old solution:
+#
+# sum = 2
+# for i in range(3,2000000,2):
+# 	sum += {True: 0, False: i}[any(i % j == 0 for j in range(3, int(i**0.5) + 1, 2))]
+# print(sum)
+###########################
+def SumPrimes(upper):
+	primes = [2,3]
+	start = 3
+	if upper%2 ==0:
+		upper = upper + 1
+
+	for i in range(start+2, upper+1, 2):
+		isPrime = True
+
+		for j in primes:
+			if i % j == 0:
+				isPrime = False
+				break
+			elif j * j > i:
+				break
+		if isPrime:
+			primes.append(i)
+	return sum(primes)
+
+print(SumPrimes(2000000))
