e6c3270c601154b60d2038f074439423ed335464
Koaxialkable <koaxialkable@gmail.com>
2015-03-12 11:34:04 -0600

    Stolen code that puts mine to shame. Beautiful.

--- Set 00/problem0010.py
+++ Set 00/problem0010.py
@@ -9,30 +9,34 @@
 #
 #	Answer: 142913828922
 ############################################
-#	Old solution:
+#	My actual, inferior solution:
 #
 # sum = 2
 # for i in range(3,2000000,2):
 # 	sum += {True: 0, False: i}[any(i % j == 0 for j in range(3, int(i**0.5) + 1, 2))]
 # print(sum)
 ###########################
-def SumPrimes(upper):
-	primes = [2,3]
-	start = 3
-	if upper%2 ==0:
-		upper = upper + 1
-
-	for i in range(start+2, upper+1, 2):
-		isPrime = True
-
-		for j in primes:
-			if i % j == 0:
-				isPrime = False
-				break
-			elif j * j > i:
-				break
-		if isPrime:
-			primes.append(i)
-	return sum(primes)
-
-print(SumPrimes(2000000))
+import time
+ 
+def prime_sum(n):
+    if n < 2: return 0
+    if n == 2: return 2
+    if n % 2 == 0: n += 1
+    primes = [True] * n
+    primes[0],primes[1] = [None] * 2 	
+    sum = 0
+    for ind,val in enumerate(primes):
+        if val is True and ind > n ** 0.5 + 1:
+            sum += ind
+        elif val is True:
+            primes[ind*2::ind] = [False] * (((n - 1)//ind) - 1)
+            sum += ind
+    return sum
+ 
+start = time.time()
+sum = prime_sum(2000000)
+elapsed = (time.time() - start)
+ 
+print ('found %s in %s seconds' % (sum,elapsed))
+# This solution was shamelessly stolen and copied from "Jason's Code Blog" at http://code.jasonbhill.com/sage/project-euler-problem-10/
+# Thanks, Jason!
